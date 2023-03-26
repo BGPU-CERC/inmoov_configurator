f1a2c0b841641321622de29a9b5a15476dd63391
readandwrite864 <63188034+readandwrite864@users.noreply.github.com>
2023-03-26 14:29:19 +0900

    refactor: move arduino to inmoov_embedded repo

--- arduino/command.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "modbus.h"
-
-#define CMD_SERVO_SET_ANGLE 10
-#define CMD_SERVO_STOP_ALL 11
-
-void command_process(TLV *tlv);
--- arduino/converter.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-union ByteArrayInteger
-{
-  unsigned char array[2];
-  int integer;
-};
-
-int bytes_to_int(unsigned char high, unsigned char low);
