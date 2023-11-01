bbac1b837b348eb9ba52f308a6f62a8302bc006a
readandwrite864 <63188034+readandwrite864@users.noreply.github.com>
2023-11-01 12:25:34 +0900

    add: config

---
+++ server/src/services/config.service.js
@@ -0,0 +1,22 @@
+import { LocalStorage } from "node-localstorage";
+const localStorage = new LocalStorage("storage");
+
+const config_file = "config.json";
+const config = {
+  lt_port: "COM1",
+  rt_port: "COM2",
+  baud_rate: 115200,
+
+  load() {
+    const item = localStorage.getItem(config_file);
+    item ? Object.assign(this, JSON.parse(item)) : this.save();
+  },
+
+  save() {
+    localStorage.setItem(config_file, JSON.stringify(this, null, 2));
+  },
+};
+
+config.load();
+
+export default config;
